3351936832b44d89e81f9b3d487a7769d9b3927f
Huang, Li-Pang <leo89214749@hotmail.com>
2018-06-28 13:30:25 +0800

    add RotatingFileHandler

--- logging_config.py
+++ logging_config.py
@@ -30,6 +30,7 @@ LOGGING = {
             "datefmt": "%Y-%m-%d %I:%M:%S %p"
         },
         "utc": {
+            # "()" is a special key, which indicates a custom instantiation.
             "()": UTCFormatter,
             "format": "%(asctime)s %(message)s",
             "datefmt": "%Y-%m-%d %I:%M:%S %p"
@@ -40,19 +41,36 @@ LOGGING = {
         "default": { 
             "level": "INFO",
             "formatter": "complete",
-            "class": "logging.StreamHandler",
+            "class": "logging.StreamHandler"
         },
-        # FileHandler will show log in file
-        "file": { 
+        # FileHandler will store log in file
+        # "file": { 
+        #     "level": "INFO",
+        #     "formatter": "complete",
+        #     "class": "logging.FileHandler",
+        #     "filename": datetime.datetime.now().strftime("%Y-%m-%d.log")
+        # },
+
+        # RotatingFileHandler will rotate the file automatically.
+        # For example, 
+        # backupCount = 5, filename = "app.log"
+        # output => app.log, app.log.1, app.log.2, up to app.log.5. 
+        # The file being written to is always app.log. When this file is filled, 
+        # it is closed and renamed to app.log.1, and if files app.log.1, app.log.2, etc. exist, 
+        # then they are renamed to app.log.2, app.log.3 etc. respectively.
+        "rotating": { 
             "level": "INFO",
             "formatter": "complete",
-            "class": "logging.FileHandler",
-            "filename": datetime.datetime.now().strftime("%Y-%m-%d.log"),
+            "class": "logging.handlers.RotatingFileHandler",
+            # need an absolute path for log file
+            "filename": "example.log",
+            "maxBytes": 1024,
+            "backupCount": 3
         }
     },
     # root logger
     "root": {
-        "handlers": ["default", "file"],
+        "handlers": ["default", "rotating"],
         "level": "INFO",
         "propagate": True
     }
